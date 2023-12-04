e72696826e268b5f8b3707356f3420e5e7451104
couscousbawl <hamdi.iadh@gmail.com>
2023-12-04 13:54:21 +0300

    creating the authentication

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
+import { LoaderFunction } from '@remix-run/node'
+import { requireUserId } from '~/utils/auth.server'
 
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request)
+  return null
+}
 
 export default function Index() {
   return (
